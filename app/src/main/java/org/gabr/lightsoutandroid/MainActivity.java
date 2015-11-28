88e74986a751f83cf438cbc6cc6e1c292e3bf6f7
Arkadiusz Gabryś <arkadiuszgabrys@gmail.com>
2015-11-28 11:34:12 +0100

    Getting movesCounter and Grid from layout. onButtonClick event for all buttons

--- app/src/main/java/org/gabr/lightsoutandroid/MainActivity.java
+++ app/src/main/java/org/gabr/lightsoutandroid/MainActivity.java
@@ -2,12 +2,24 @@ package org.gabr.lightsoutandroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.GridLayout;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+    private TextView movesCounter;
+    private GridLayout grid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        movesCounter = (TextView) findViewById(R.id.movesCounter);
+        grid = (GridLayout) findViewById(R.id.grid);
+    }
+
+    public void onButtonClick(View view) {
     }
 }
